e839484e1b1f060f5ac2deb4708c3cd107947287
darrpama <darrpama@darrpamas-MacBook-Air.local>
2024-12-02 14:50:51 +0300

    add Application db context changes

--- src/APIV1/APIV1/Models/ApplicationDbContext.cs
+++ src/APIV1/APIV1/Models/ApplicationDbContext.cs
@@ -5,16 +5,26 @@ namespace APIV1.Models;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<Client> Clients { get; set; } = null!;
-    public DbSet<Product> Products { get; set; } = null!;
-    public DbSet<Supplier> Suppliers { get; set; } = null!;
-    public DbSet<Image> Images { get; set; } = null!;
-    public DbSet<Address> Addresses { get; set; } = null!;
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<Image> Images { get; set; }
+    public DbSet<Address> Addresses { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        Database.EnsureDeleted();
-        Database.EnsureCreated();
+        // builder.Entity<Client>()
+        //     .HasOne(c => c.Address)
+        //     .WithOne(a => a.Client)
+        //     .OnDelete(DeleteBehavior.Cascade); // Cascade delete for Client
+
+        // builder.Entity<Supplier>()
+        //     .HasOne(s => s.Address)
+        //     .WithOne(a => a.Supplier)
+        //     .OnDelete(DeleteBehavior.Cascade); // Cascade delete for Supplier
     }
 }
